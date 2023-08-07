6884dc72fddcc61ba0bf4a74b0a72965880893c1
sazzad-anwar <sazzadzihan@gmail.com>
2023-08-07 15:33:12 +0600

    added log to google signIn

--- lib/auth.ts
+++ lib/auth.ts
@@ -60,21 +60,16 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async signIn({ account, user }) {
       if (account?.provider === "google") {
-        try {
-          let res = await fetch(
-            `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/auth/google/callback?id_token=${account?.id_token}&access_token=${account?.access_token}`
-          )
-          let data = await res.json()
-          user.token = data?.jwt
-          user.email = data?.user?.email
-          user.id = data?.user?.id
-          user.name = data?.user?.username
-          cookies().set("token", data?.jwt, { maxAge: 60 * 60 * 24 * 30 })
-          return true
-        } catch (error: any) {
-          console.log(error)
-          throw new Error(error?.message)
-        }
+        let res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/auth/google/callback?id_token=${account?.id_token}&access_token=${account?.access_token}`
+        )
+        let data = await res.json()
+        user.token = data?.jwt
+        user.email = data?.user?.email
+        user.id = data?.user?.id
+        user.name = data?.user?.username
+        cookies().set("token", data?.jwt, { maxAge: 60 * 60 * 24 * 30 })
+        return true
       }
       return true
     },
