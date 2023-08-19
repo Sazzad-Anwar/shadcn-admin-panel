05ad80b680729b818e81b270da9f34c56a377651
sazzad-anwar <sazzadzihan@gmail.com>
2023-08-19 17:00:48 +0600

    updated metadata of admin layout

--- components/login/page.tsx
+++ components/login/page.tsx
@@ -1,31 +1,12 @@
 "use client"
 
 import React from "react"
-import { Metadata } from "next"
 import { redirect } from "next/navigation"
 import { signIn, useSession } from "next-auth/react"
 
 import { AppRoutes } from "@/config/routes"
-import { siteConfig } from "@/config/site"
 
 import { Button } from "../ui/button"
-
-export const metadata: Metadata = {
-  title: {
-    default: "Login",
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: "Amdin panel",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
 
 export default function Login() {
   let session = useSession()
